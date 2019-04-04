980cfa9dff931ad216bcc1a8f97947b6ad02be43
Tulio Faria <tuliofaria@gmail.com>
2019-04-04 19:35:41 -0300

    with db.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const sqlite = require('sqlite')
+
+const dbPromise = sqlite.open('./chinook.db', { Promise })
 
 app.get('/', (req, res) => {
   res.send('Olá! <a href="/about">About page</a>')
 })
 app.get('/about', (req, res) => {
   res.send('Olá about!')
+})
+app.get('/artists', async(req, res) => {
+  const db = await dbPromise
+  const artists = await db.all('SELECT * FROM artists')
+  res.send(artists)
 })
 
 app.listen(process.env.PORT || 3000, err => console.log(err))
