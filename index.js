80bd19adeac1ee11bd419669eb5a48ffdd6a2094
Tulio Faria <tuliofaria@gmail.com>
2019-04-04 14:52:07 -0300

    initial version.

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Olá!')
+})
+
+app.listen(process.env.PORT || 3000, (err) => {
+  console.log('listening...')
+})
